fff681834bffa8150e361fa29d3dd8c8d59b2342
magical <mountainsaremagic@gmail.com>
2024-08-30 23:16:56 +0530

    changes

--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TagModule } from 'primeng/tag';
+import { FormBuilder, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
+import { CalendarModule } from 'primeng/calendar';
+import { DataserviceService } from '../services/dataservice.service';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [TagModule], // This should include GMapModule
+  imports: [TagModule, ReactiveFormsModule, CalendarModule,ToastModule, ButtonModule, RippleModule], // This should include GMapModule
   templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.scss'] // Corrected to styleUrls
+  styleUrls: ['./contact-us.component.scss'], // Corrected to styleUrls
+  providers: [MessageService]
 })
 export class ContactUsComponent {
+  contactFrom: any = new FormGroup({});
+  formData!: any[];
+  private dataService = inject(DataserviceService);
+  private _fb = inject(FormBuilder);
+  constructor(private messageService: MessageService) {}
+
+  ngOnInit() {
+    this.formData = this.dataService.getContactFormData();
+    this.bindControls();
+  }
+
+  bindControls() {
+    let validators: ValidatorFn[] = [];
+    this.formData.forEach(control => {
+      if (control?.validation_rule) {
+        validators = [];
+        validators = this.getValidationRules(control);
+        const formControl = this._fb.control(null, validators);
+        this.contactFrom.addControl(control.control_name, formControl);
+      }
+    })
+  }
+
+  getValidationRules(element: any) {
+    let validators: any = [];
+    element.validation_rule.forEach((val: any) => {
+      if (val.mandation) validators.push(Validators.required)
+      if (val.min) validators.push(Validators.minLength(val.min.column_minlength as number))
+      if (val.max) validators.push(Validators.maxLength(val.max.column_maxlength as number))
+      if (val.mobile) validators.push(Validators.pattern(/^\+?\d{10,15}$/));
+      if (val.email) validators.push(Validators.email);
+    });
+    return validators;
+  }
+  logFormValues(){
+    this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Form Submission sucessfull' })
+  }
 }
--- src/app/components/services/dataservice.service.ts
+++ src/app/components/services/dataservice.service.ts
@@ -31,7 +31,7 @@ export class DataserviceService {
         id: 1,
         name: "Solo Trip to Kashmir",
         price: "53,000",
-        image: "https://kashmirtravelportal.com/wp-content/uploads/2024/07/vkkg9xouczi.jpg",
+        image: "assets/banner-1.jpg",
         description: "Conquer snow-capped peaks, glacial lakes, and lush valleys.",
         days: 5,
         nights: 4,
@@ -94,7 +94,7 @@ export class DataserviceService {
             ]
           },
         ],
-        "inclusions" : [
+        "inclusions": [
           "A cosy and hygienic car (Innova/Xylo, or a comparable model) for sightseeing on each of the days specified in the schedule.",
           "According to the timing of your journey, airport pickup and drop-off.",
           "Stay in clean, well-maintained lodgings with double and triple occupancy.",
@@ -118,37 +118,73 @@ export class DataserviceService {
         activities: [
           {
             "day": "Airport to Srinagar",
-            "dayActivity": "Site Seeing",
-            "return": true
+            "dayActivity": [
+              "A representative will meet you at the prearranged location in Srinagar and provide a brief overview of the entire trip.",
+              "After that, a motel in Srinagar will be where you are taken.",
+              "Finish the check-in procedures, take some time to relax, and then go exploring Srinagar.",
+              "Check out the Shalimar Bagh and Nishat Bagh, two of the most well-known Mughal Gardens (abode of love).",
+              "The Zabarwan hills can be seen in the distance as these gardens encircle Dal Lake's eastern side for several kilometres.",
+              "Spend some time by the lake in the evening and look around the neighbourhood."
+            ]
           },
           {
             "day": "Srinagar to sonamarg",
-            "dayActivity": " Exploring Sonamarg",
-            "return": true
+            "dayActivity": [
+              "The hotel's delicious brunch is served each morning with a stunning view of Dal Lake.",
+              "Prepare yourself for the ultimate journey to Sonamarg after breakfast.",
+              "Set out on a full-day trip to the lovely Sonmarg 'meadow of gold.'",
+              "Stop at picturesque locations along the way to Sonamarg to take in the scenery and the surrounding natural beauty.",
+              "When you get to Sonamarg, you can take a horse to the Thajiwas Glacier, where snow is present all year long.",
+              "In Sonamarg, you can also take part in the most well-liked sports activities, like white water kayaking.",
+              "Drive back to your accommodation in Srinagar after finishing your exploration of Sonamarg.",
+              "Enjoy a night's sleep and supper at the Srinagar hotel."
+            ]
           },
           {
             "day": "Srinagar to Pahalgam",
-            "dayActivity": " Exploring Betaab, Aru Chandanwari",
-            "return": true
+            "dayActivity": [
+              "Enjoy a delicious breakfast while taking in the stunning view of the snow-capped mountains before checking out of the hotel.",
+              "You will then continue on to Pahalgam, also known as the shepherds' region.",
+              "Once you arrive in Pahalgam, settle into your hotel and take some time to unwind before travelling to the lovely Aru Valley.",
+              "A small hamlet called Aru Valley is renowned for its scenic beauty and offers a variety of adventurous activities.",
+              "The Betaab Valley, one of the most well-known tourist locations that offers breathtaking views of the Lidder River while being surrounded by heavenly natural charm, should then be reached.",
+              "Additionally, Chandanwari, which is known for its snow bridge and serves as the beginning point of the revered Amarnath Yatra, will be on your itinerary.",
+              "Return to the hotel in Pahalgam and call it a day after a day filled with breathtaking sightseeing and exhilarating experiences."
+            ]
           },
           {
             "day": "Srinagar to Gulmarg",
-            "dayActivity": "Exploring Gulmarg",
-            "return": true
+            "dayActivity": [
+              "Start your day early with a hearty breakfast, then set off on a scenic drive from Srinagar to Gulmarg.",
+              "Upon reaching Gulmarg, your main activity will be the Gondola Ride, offering stunning panoramic views of the snow-covered mountains.",
+              "As the cable car rises, enjoy the sweeping vistas of the valley below, revealing the hidden beauty of nature.",
+              "Before heading back to Srinagar, take advantage of Gulmarg’s famous ski slopes and try your hand at adventure sports like skiing and paragliding.",
+              "On the return journey, savor the natural beauty and snow-capped mountains that have accompanied you throughout the day.",
+              "Enjoy dinner and an overnight stay at a hotel in Srinagar."
+            ]
           },
           {
             "day": "Srinagar to Airport",
-            "dayActivity": "Leaving with beautiful Memories",
-            "return": true
+            "dayActivity": [
+              "Begin your day with a satisfying breakfast, taking in the breathtaking view of the snow-capped mountains before checking out of the houseboat.",
+              "The tour concludes as you are dropped off at a preferred location in Srinagar."
+            ]
           },
         ],
-        inclusions: ["5 Night accomdation", "aily breakfast and dinner", "pick and drop", "1 shikara ride"]
+        "inclusions": [
+          "A cosy and hygienic car (Innova/Xylo, or a comparable model) for sightseeing on each of the days specified in the schedule.",
+          "According to the timing of your journey, airport pickup and drop-off.",
+          "Stay in clean, well-maintained lodgings with double and triple occupancy.",
+          "Food included, per the schedule.",
+          "Professional chauffeur cum guide.",
+          "Sightseeing as per the schedule."
+        ]
       },
       {
         id: 3,
         name: "Kashmir Bliss",
         price: "73500",
-        image: "https://kashmirtravelportal.com/wp-content/uploads/2024/07/vv0ff4eozai.jpg",
+        image: "assets/banner-2.jpg",
         description: "Immerse yourself in the tranquil beauty of Ladakh",
         days: 6,
         nights: 5,
@@ -189,7 +225,7 @@ export class DataserviceService {
           },
         ],
 
-        "inclusions" : [
+        "inclusions": [
           "A cosy and hygienic car (Innova/Xylo, or a comparable model) for sightseeing on each of the days specified in the schedule.",
           "According to the timing of your journey, airport pickup and drop-off.",
           "Stay in clean, well-maintained lodgings with double and triple occupancy.",
@@ -197,7 +233,7 @@ export class DataserviceService {
           "Professional chauffeur cum guide.",
           "Sightseeing as per the schedule."
         ]
-            },
+      },
       {
         id: 4,
         name: "Kashmir Together",
@@ -235,7 +271,7 @@ export class DataserviceService {
             "dayActivity": "Leaving with beautiful Memories"
           },
         ],
-        "inclusions" : [
+        "inclusions": [
           "A cosy and hygienic car (Innova/Xylo, or a comparable model) for sightseeing on each of the days specified in the schedule.",
           "According to the timing of your journey, airport pickup and drop-off.",
           "Stay in clean, well-maintained lodgings with double and triple occupancy.",
@@ -292,7 +328,7 @@ export class DataserviceService {
             "dayActivity": "Leaving with beautiful Memories"
           },
         ],
-        "inclusions" : [
+        "inclusions": [
           "A cosy and hygienic car (Innova/Xylo, or a comparable model) for sightseeing on each of the days specified in the schedule.",
           "According to the timing of your journey, airport pickup and drop-off.",
           "Stay in clean, well-maintained lodgings with double and triple occupancy.",
@@ -343,7 +379,7 @@ export class DataserviceService {
         ],
         // inclusions: ["6 Night accomdation", "Daily breakfast and Dinner", "pick and drop", "1 shikara ride"]
 
-        "inclusions" : [
+        "inclusions": [
           "A cosy and hygienic car (Innova/Xylo, or a comparable model) for sightseeing on each of the days specified in the schedule.",
           "According to the timing of your journey, airport pickup and drop-off.",
           "Stay in clean, well-maintained lodgings with double and triple occupancy.",
@@ -419,7 +455,7 @@ export class DataserviceService {
             "dayActivity": "Experience the beauty of Pangong Lake with our camping package! Enjoy a scenic drive from Leh to the stunning blue waters of Pangong, where you'll set up camp in comfortable tents. Spend your days exploring the lake, taking leisurely walks along the shore, and soaking in the breathtaking views."
           },
         ],
-        "inclusions" : [
+        "inclusions": [
           "A cosy and hygienic car (Innova/Xylo, or a comparable model) for sightseeing on each of the days specified in the schedule.",
           "According to the timing of your journey, airport pickup and drop-off.",
           "Stay in clean, well-maintained lodgings with double and triple occupancy.",
@@ -431,11 +467,126 @@ export class DataserviceService {
       },
     ]
   }
-
-
-
-
   getProductsSmall() {
     return Promise.resolve(this.getProductsData().slice(0, 10));
+  }
+
+  getContactFormData() {
+    return [
+      {
+        "control_type": 'text',
+        "control_name": "name",
+        "control_label": "Name",
+        "placeholder": 'Your Name',
+        "validation_rule": [
+          {
+            "mandation": {
+              "error": "please supply a value",
+              "is_mandatory": true
+            }
+          },
+          {
+            "min": {
+              "error": "cannot be less than 1 character",
+              "column_minlength": "1"
+            }
+          },
+          {
+            "max": {
+              "error": "cannot exceed 255 characters",
+              "column_maxlength": "255"
+            }
+          }
+        ],
+      },
+      {
+        "control_type": 'number',
+        "control_name": "phone",
+        "control_label": "Mobile Number",
+        "placeholder": 'Mobile Number',
+        "validation_rule": [
+          {
+            "mandation": {
+              "error": "please supply a value",
+              "is_mandatory": true
+            }
+          },
+          {
+            "min": {
+              "error": "cannot be less than 1 character",
+              "column_minlength": "1"
+            }
+          },
+          {
+            "max": {
+              "error": "cannot exceed 255 characters",
+              "column_maxlength": "255"
+            }
+          },
+          {
+            "phone": {
+              'validation':true
+            }
+          }
+        ],
+      },
+      {
+        "control_type": 'email',
+        "control_name": "email",
+        "control_label": "Email",
+        "placeholder": 'Your Email ID',
+        "validation_rule": [
+          {
+            "mandation": {
+              "error": "please supply a value",
+              "is_mandatory": true
+            }
+          },
+          {
+            "min": {
+              "error": "cannot be less than 1 character",
+              "column_minlength": "1"
+            }
+          },
+          {
+            "max": {
+              "error": "cannot exceed 255 characters",
+              "column_maxlength": "255"
+            }
+          },
+          {
+            "email": {
+              'validation':true
+            }
+          }
+        ],
+      },
+      {
+        "control_type": 'textarea',
+        "control_name": "message",
+        "control_label": "Your Message",
+        "placeholder": 'Your Email ID',
+        "validation_rule": [
+          {
+            "mandation": {
+              "error": "please supply a value",
+              "is_mandatory": true
+            }
+          },
+          {
+            "min": {
+              "error": "cannot be less than 1 character",
+              "column_minlength": "1"
+            }
+          },
+          {
+            "max": {
+              "error": "cannot exceed 255 characters",
+              "column_maxlength": "255"
+            }
+          }
+        ],
+      },
+    ]
   }
 }
